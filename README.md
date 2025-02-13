fc60becafcb9302088577edd262069ab23a483a9
Hoang Phat Vu <vhphatdz@gmail.com>
2025-02-14 00:50:36 +0700

    fix: wrong path name and readme instruction

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ python compute_mean_std.py /path/to/data1 /path/to/data2 --batch_size 100
 ```
 
 ```shell
-!bash tools/dist_finetune.sh projects/configs/custom/fisheye8k.py 2 output --cfg-options $(cat projects/configs/tuning/fisheye8k.txt)
+!bash tools/dist_finetune.sh projects/configs/custom/fisheye8k.py 2 output $(cat projects/configs/tuning/fisheye8k.txt)
 ```
--- tools/dist_finetune.sh
+++ tools/dist_finetune.sh
@@ -9,15 +9,15 @@ echo $PYTHONPATH
 
 EXTRA_ARGS=""
 if [ -f "$CONFIG" ]; then
-    NUM_CLASSES=$(python3 tools/train_utils/get_nc.py "$CONFIG")
+    NUM_CLASSES=$(python3 $(dirname "$0")/train_utils/get_nc.py "$CONFIG")
 
     if [ "$NUM_CLASSES" -gt 0 ]; then
-        HEADS=$(python3 tools/train_utils/get_nc_configs.py "$CONFIG" "$NUM_CLASSES")
+        HEADS=$(python3 $(dirname "$0")/train_utils/get_nc_config.py "$CONFIG" "$NUM_CLASSES")
         if [ -n "$HEADS" ]; then
             EXTRA_ARGS="--cfg-options $HEADS"
         fi
     fi
 fi
-
+ 
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch --work-dir $WORKDIR $EXTRA_ARGS "${@:4}"
